a7c76c05ab0375d8b8321d8fd8cd8ac769aff8cb
schm00g <hk8igureu@relay.firefox.com>
2022-04-07 00:31:38 +0100

    typeof

--- dataTypes.js
+++ dataTypes.js
@@ -6,5 +6,25 @@ console.log("hello" / 2) // NaN, such division is erroneous
 
 /*  JavaScript is “dynamically typed” */
 
-const bigInt = 67854356787654657899765n;
+const bigInt = 67854356787654657899765n; // primitive
 console.log(bigInt)
+
+let isGreater = 4 > 1;
+console.log(isGreater) // true
+
+let age = null; // “nothing”, “empty” or “value unknown”
+
+let name;
+console.log(name) // undefined === 'value not defined'
+
+/* objects can store collections of more complex data */
+
+console.log(typeof undefined)
+console.log(typeof 0)
+console.log(typeof 10000n)
+console.log(typeof false)
+console.log(typeof "foo")
+console.log(typeof Symbol("id"))
+console.log(typeof Math)
+console.log(typeof null)
+console.log(typeof alert)
