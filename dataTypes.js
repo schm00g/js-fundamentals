cf0b74b6a765d87081a475ae9a062bfcf8dcea0d
schm00g <hk8igureu@relay.firefox.com>
2022-04-07 00:35:41 +0100

    all data types

--- dataTypes.js
+++ dataTypes.js
@@ -19,6 +19,8 @@ console.log(name) // undefined === 'value not defined'
 
 /* objects can store collections of more complex data */
 
+// typeof is an operator, not a function
+
 console.log(typeof undefined)
 console.log(typeof 0)
 console.log(typeof 10000n)
@@ -26,5 +28,19 @@ console.log(typeof false)
 console.log(typeof "foo")
 console.log(typeof Symbol("id"))
 console.log(typeof Math)
-console.log(typeof null)
+console.log(typeof null) // null returns "object" – this is an error in the language, it’s not actually an object.
 console.log(typeof alert)
+
+/*
+    8 basic data types in JavaScript:
+
+    number for numbers of any kind: integer or floating-point, integers are limited by ±(253-1).
+    bigint is for integer numbers of arbitrary length.
+    string for strings. A string may have zero or more characters, there’s no separate single-character type.
+    boolean for true/false.
+    null for unknown values – a standalone type that has a single value null.
+    undefined for unassigned values – a standalone type that has a single value undefined.
+    object for more complex data structures.
+    symbol for unique identifiers.
+
+*/
