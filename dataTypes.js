2462c4128dd79aa2d86fdeabaf9321e56a9b62cd
schm00g <hk8igureu@relay.firefox.com>
2022-04-07 00:23:35 +0100

    data types

---
+++ dataTypes.js
@@ -0,0 +1,10 @@
+console.log(1 / 0) // Infinity
+
+console.log(Infinity)
+
+console.log("hello" / 2) // NaN, such division is erroneous
+
+/*  JavaScript is “dynamically typed” */
+
+const bigInt = 67854356787654657899765n;
+console.log(bigInt)
