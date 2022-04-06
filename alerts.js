4bc80219da6a4e35edb927d3d42cd7d679ceca2d
schm00g <hk8igureu@relay.firefox.com>
2022-04-07 00:45:06 +0100

    browser specific funcs

---
+++ alerts.js
@@ -0,0 +1,7 @@
+// browser-specific functions to interact with visitors
+alert("Yurt!");
+
+result = prompt("How old are you?", 100);
+alert(`User is ${result} years old.`);
+
+check = confirm("Are you okay?");
