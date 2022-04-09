deed291ed1286d3c0e55214082aa5babbc73f044
schm00g <hk8igureu@relay.firefox.com>
2022-04-09 19:38:10 +0100

    strict equality

--- comparisons.js
+++ comparisons.js
@@ -16,3 +16,11 @@ console.log('Z' > 'A') // true
 console.log('Glow' < 'Glee') // false
 
 console.log('2' > 1) // true (when types differ JS converts values to numbers)
+
+console.log(true == 1) // true becomes 1
+console.log(false == 0) // false becomes 0
+
+// === is required for strict equality
+console.log(0 == false)
+console.log('' == false)
+console.log(0 === false)
