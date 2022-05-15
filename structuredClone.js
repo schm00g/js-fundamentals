eee5677e2649464647d95dc00a7f6433dd06a017
schm00g <hk8igureu@relay.firefox.com>
2022-05-15 14:49:41 +0100

    shallow clone with ...spread

--- structuredClone.js
+++ structuredClone.js
@@ -8,3 +8,27 @@ const clone = structuredClone(original);
 console.assert(clone !== original); // objects not same identity
 console.assert(clone.name === "MDN"); // have same values
 console.assert(clone.itself === clone); // circular reference preserved
+
+/* 	Copying a value in JavaScript is almost always 
+ 	shallow, as opposed to deep. That means that 
+ 	changes to deeply nested values will be visible
+ 	in the copy as well as the original. */
+
+// spreads are most often used for shallow copies
+const myOriginal = {
+	someProp: "with a string",
+	anotherProp: {
+		a: 1,
+		b: 2
+	}
+}
+
+const myShallowCopy = {...myOriginal};
+
+myShallowCopy.aNewProp = "a new value";
+console.log(myOriginal.aNewProp); // undefined
+
+/* 	adding or changing a deeply nested property
+ 	affects both the copy and the original */
+myShallowCopy.anotherProp.c = "another new value";
+console.log(myOriginal.anotherProp.c);
